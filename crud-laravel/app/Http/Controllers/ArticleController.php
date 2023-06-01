2c758d097ba19802a0d6e3b3272f9f7064f8c320
arifsptra <arif.sptrra@gmail.com>
2023-06-01 23:55:58 +0700

    make create read

--- crud-laravel/app/Http/Controllers/ArticleController.php
+++ crud-laravel/app/Http/Controllers/ArticleController.php
@@ -2,9 +2,29 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Article;
 use Illuminate\Http\Request;
 
 class ArticleController extends Controller
 {
-    //
+    public function index() {
+        return view('article.index', [
+            'articles' => Article::get()
+        ]);
+    }
+
+    public function create() {
+        return view('article.form');
+    }
+
+    public function store(Request $request) {
+        $inputs = $request->only(['title', 'description']);
+        $create = Article::create($inputs);
+
+        if($create) {
+            return redirect()->route('article.index');
+        }
+
+        return abort(500);
+    }
 }
